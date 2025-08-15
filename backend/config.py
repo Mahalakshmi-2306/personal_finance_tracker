6ff344cac4453b4e43077eb5a896ab238a18c1bf
Mahalakshmi-2306 <mahalakshmi.jainam@gmail.com>
2025-08-15 12:04:56 +0530

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,6 +1,6 @@
 DB_CONFIG = {
     'host': 'localhost',
     'user': 'root',
-    'password': 'vijay@19_98',  # replace with your MySQL password
+    'password': ' ',  # replace with your MySQL password
     'database': 'finance_tracker'
 }
